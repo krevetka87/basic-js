f8e20590cd89755d790596463800f46888f0554d
krevetka87 <bonanast4sia@yandex.ru>
2021-02-22 19:39:15 +0300

    add solutions count-cats.js, dream-team.js, what-season.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    return members.filter(e => typeof e === 'string').map(e => e.trim().charAt(0).toUpperCase()).sort().join('');
+  }
+  else {
+    return false;
+  }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  else if (toString.call(date) != '[object Date]') {
+    throw new Error('THROWN');
+  }
+  else {
+    let month = date.getMonth();
+    if (month === 0 || month === 1 || month === 11) {
+      return 'winter';
+    }
+    else if (month === 2 || month === 3 || month === 4) {
+      return 'spring';
+    }
+    else if (month === 5 || month === 6 || month === 7) {
+      return 'summer';
+    }
+    else {
+      return 'autumn';
+    }
+  }
+
 };
