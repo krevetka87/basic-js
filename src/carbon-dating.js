b13bf84543d6abcad5cd752a7014442a0e566cc4
krevetka87 <bonanast4sia@yandex.ru>
2021-03-25 00:59:32 +0300

    feat: add solution to carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,9 +3,22 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  sampleActivity = Number(sampleActivity) || null;
-  if (typeof sampleActivity != 'number' || sampleActivity < 0 || sampleActivity === null) {
+module.exports = function dateSample(sampleActivity) {  
+  if (sampleActivity == undefined || typeof sampleActivity != 'string') {
     return false;
   }
+  sampleActivity = Number(sampleActivity);
+  if(isNaN(sampleActivity) || sampleActivity === 0){
+    return false;
+  }
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) / k);
+  if (t < 0 || isNaN(t)){
+    return false;
+  }
+  return t;
 };
+
+
+
+
