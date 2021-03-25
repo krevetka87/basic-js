a89389a989eb1f0a7715a4204bd82d6ec6589a72
krevetka87 <bonanast4sia@yandex.ru>
2021-03-25 19:18:45 +0300

    feat: add solution to recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,12 @@
 const CustomError = require("../extensions/custom-error");
-
+const checkElements = (arr) => arr.some(element => Array.isArray(element));
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-    retun
-
+  calculateDepth(arr){
+    let countDepth = 1;
+    if(checkElements(arr)){
+      let arr2 = arr.flat();
+      return countDepth + this.calculateDepth(arr2);
+    }
+    return countDepth;
   }
 };
