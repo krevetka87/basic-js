ae901bc3722369d9f566e042cb7a3b306971faf4
krevetka87 <bonanast4sia@yandex.ru>
2021-03-28 00:49:20 +0300

    feat: add solution to transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,43 +7,25 @@ module.exports = function transform(arr) {
   if (!arr.includes('--discard-next') && !arr.includes('--discard-prev') && !arr.includes('--double-next') && !arr.includes('--double-prev')) {
     return arr;
   }
-  let newArr = Array.from(arr);
-  for (let i = 0; i < newArr.length; i++){
-    if(newArr[i] === '--discard-next'){
-      if(i === newArr.length-1){
-        newArr.pop();
-      }
-      else if (newArr[i+2] === '--discard-prev'){
-        newArr.splice(i, 1, '')
-      }
-      else {
-        newArr.splice(i, 2, '', '')
-      }
-    }
-    if(newArr[i] === '--discard-prev'){
-      if(i === 0){
-        newArr.splice(i, 1, '');
-      }
-      else {
-        newArr.splice(i-1, 2, '', '')
-      }
-    }
-    if(newArr[i] === '--double-next'){
-      if(i === newArr.length-1){
-        newArr.pop();
-      }
-      else {
-        newArr.splice(i, 1, newArr[i+1]);
-      }
-    }
-    if(newArr[i] === '--double-prev'){
-      if(i === 0){
-        newArr.splice(i, 1);
-      }
-      else {
-      newArr.splice(i, 1, newArr[i-1]);
-      }
-    }
-  }
-  return newArr.filter(e => e != '');
+  let transform = [];
+  
+  for (let i = 0; i < arr.length; i++){
+   if(arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != '--double-next' && arr[i] != '--double-prev'){
+     transform.push(arr[i]);
+     if(arr[i+1] === '--double-prev' && arr[i-1] != '--discard-next' && arr[i] != undefined){
+       transform.push(arr[i]);
+     }
+     if(arr[i+1] === '--discard-prev' && arr[i-1] != '--discard-next' && arr[i] != undefined){
+       transform.pop();
+     }
+     if(arr[i-1] === '--double-next' && arr[i] != undefined){
+       transform.push(arr[i]);
+     }
+     if(arr[i-1] === '--discard-next' && arr[i] != undefined){
+       transform.pop();
+     }
+   }
+    
+}
+return transform;
 }
